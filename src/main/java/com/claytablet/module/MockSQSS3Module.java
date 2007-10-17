2972a09d1737a61bfc2cbe5e96d8749d6de2c6d4
rapind <rapind@9b97ad21-233c-0410-b11d-29043ffab824>
2007-10-17 16:30:21 +0000

    added a mock receiver implementation

---
+++ src/main/java/com/claytablet/module/MockSQSS3Module.java
@@ -0,0 +1,27 @@
+package com.claytablet.module;
+
+import com.claytablet.service.event.ProviderReceiver;
+import com.claytablet.service.event.mock.ProviderReceiverMock;
+
+/**
+ * Copyright 2007 Clay Tablet Technologies Inc.
+ * 
+ * <p>
+ * 
+ * @author <a href="mailto:drapin@clay-tablet.com">Dave Rapin</a>
+ * 
+ * <p>
+ * Mock module for Guice configuration. Extends the SQSS3Module and overrides
+ * the default receiver binding for a mock implementation.
+ */
+public class MockSQSS3Module extends SQSS3Module {
+
+	protected void configure() {
+
+		super.configure();
+
+		// override the default receiver binding
+		bind(ProviderReceiver.class).to(ProviderReceiverMock.class);
+
+	}
+}
---
+++ src/main/java/com/claytablet/service/event/mock/ProviderReceiverMock.java
@@ -0,0 +1,189 @@
+package com.claytablet.service.event.mock;
+
+import com.claytablet.factory.QueuePublisherServiceFactory;
+import com.claytablet.factory.StorageClientServiceFactory;
+import com.claytablet.model.event.platform.ApprovedAssetTask;
+import com.claytablet.model.event.platform.CanceledAssetTask;
+import com.claytablet.model.event.platform.ProcessingError;
+import com.claytablet.model.event.platform.RejectedAssetTask;
+import com.claytablet.model.event.platform.StartAssetTask;
+import com.claytablet.service.event.EventServiceException;
+import com.claytablet.service.event.ProviderReceiver;
+import com.claytablet.service.event.ProviderSender;
+import com.claytablet.service.event.impl.AbsEventClientImpl;
+import com.claytablet.storage.service.StorageClientService;
+import com.claytablet.storage.service.StorageServiceException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * Copyright 2007 Clay Tablet Technologies Inc.
+ * 
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at <a
+ * href="http://www.apache.org/licenses/LICENSE-2.0">http://www.apache.org/licenses/LICENSE-2.0</a>
+ * 
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * <p>
+ * 
+ * @author <a href="mailto:drapin@clay-tablet.com">Dave Rapin</a>
+ * 
+ * <p>
+ * This is the default implementation for the provider receiver.
+ * 
+ * <p>
+ * @see StorageClientService
+ */
+@Singleton
+public class ProviderReceiverMock extends AbsEventClientImpl implements
+		ProviderReceiver {
+
+	// we're going to automatically respond to messages
+	private ProviderSender providerSender;
+
+	private static final String PLATFORM_ACCOUNT_ID = "ctt-platform";
+
+	// TODO - replace this with your assigned account identifier
+	private static final String PROVIDER_ACCOUNT_ID = "ctt-provider-tms1";
+
+	/**
+	 * Constructor for dependency injection.
+	 * 
+	 * @param queuePublisherServiceFactory
+	 * @param storageClientServiceFactory
+	 * @param providerSender
+	 */
+	@Inject
+	public ProviderReceiverMock(
+			QueuePublisherServiceFactory queuePublisherServiceFactory,
+			StorageClientServiceFactory storageClientServiceFactory,
+			ProviderSender providerSender) {
+		this.queuePublisherServiceFactory = queuePublisherServiceFactory;
+		this.storageClientServiceFactory = storageClientServiceFactory;
+		this.providerSender = providerSender;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.claytablet.service.event.ReceiverProvider#receiveEvent(com.claytablet.model.event.producer.ApprovedAssetTask)
+	 */
+	public void receiveEvent(ApprovedAssetTask event)
+			throws StorageServiceException, EventServiceException {
+
+		log.debug(event.getClass().getSimpleName() + " event received.");
+
+		// check to see if new content was submitted with the event
+		if (event.isWithContent()) {
+			log.debug("A new asset task revision was sent with the approval.");
+
+			log.debug("Download the latest asset task revision for: "
+					+ event.getAssetTaskId());
+			String downloadPath = super.downloadLatestAssetTaskVersion(event
+					.getTargetAccountId(), event.getAssetTaskId(),
+					"./files/received/");
+
+			log.debug("Downloaded an asset task version file to: "
+					+ downloadPath);
+		}
+
+		// TODO - provider integration code goes here.
+
+		// If an exception is thrown the event will remain on the queue.
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.claytablet.service.event.ReceiverProvider#receiveEvent(com.claytablet.model.event.producer.CanceledAssetTask)
+	 */
+	public void receiveEvent(CanceledAssetTask event) {
+
+		log.debug(event.getClass().getSimpleName() + " event received.");
+
+		// TODO - provider integration code goes here.
+
+		// If an exception is thrown the event will remain on the queue.
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.claytablet.service.client.ReceiverProvider#receiveEvent(com.claytablet.model.event.platform.ProcessingError)
+	 */
+	public void receiveEvent(ProcessingError event) {
+
+		log.debug(event.getClass().getSimpleName() + " event received.");
+
+		// do nothing
+
+		log.error(event.getErrorMessage());
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.claytablet.service.event.ReceiverProvider#receiveEvent(com.claytablet.model.event.producer.RejectedAssetTask)
+	 */
+	public void receiveEvent(RejectedAssetTask event)
+			throws StorageServiceException, EventServiceException {
+
+		log.debug(event.getClass().getSimpleName() + " event received.");
+
+		// check to see if new content was submitted with the event
+		if (event.isWithContent()) {
+			log.debug("A new asset task revision was sent with the rejection.");
+
+			log.debug("Download the latest asset task revision for: "
+					+ event.getAssetTaskId());
+			String downloadPath = super.downloadLatestAssetTaskVersion(event
+					.getTargetAccountId(), event.getAssetTaskId(),
+					"./files/received/");
+
+			log.debug("Downloaded an asset task version file to: "
+					+ downloadPath);
+		}
+
+		// TODO - provider integration code goes here.
+
+		// If an exception is thrown the event will remain on the queue.
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.claytablet.service.client.ReceiverProvider#receiveEvent(com.claytablet.model.event.platform.StartAssetTask)
+	 */
+	public void receiveEvent(StartAssetTask event)
+			throws StorageServiceException, EventServiceException {
+
+		log.debug(event.getClass().getSimpleName() + " event received.");
+
+		log.debug("Download the latest asset task revision for: "
+				+ event.getAssetTaskId());
+		String downloadPath = super.downloadLatestAssetTaskVersion(event
+				.getTargetAccountId(), event.getAssetTaskId(),
+				"./files/received/");
+
+		log.debug("Downloaded an asset task version file to: " + downloadPath);
+
+		// TODO - provider integration code goes here.
+		// I.e. send the asset to the TMS and mark it as pending.
+
+		// I.e. call an external system method called new document
+
+		// If an exception is thrown the event will remain on the queue.
+
+	}
+
+}
